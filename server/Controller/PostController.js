b080ce6fcc2a92b2c7862e8187d66a0e1654e091
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-09-08 08:21:34 +0300

    Revert "first-commit"
    This reverts commit 877c477e72c95d58c55079b2a63a0cd28ad908e5.

--- server/Controller/PostController.js
+++ server/Controller/PostController.js
@@ -25,42 +25,22 @@ export const CreatePost = async (req, res) => {
 }
 export const GetFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
+    
+  } catch (error) {
+    
   }
 }
 export const GetUsersPosts = async (req, res) => {
   try {
-    const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
+    
+  } catch (error) {
+    
   }
 }
 export const LikePost = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { userId } = req.body;
-    const post = await Post.findById(id);
-    const isLiked = post.likes.get(userId);
-
-    if (isLiked) {
-      post.likes.delete(userId);
-    } else {
-      post.likes.set(userId, true);
-    }
-
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      { likes: post.likes },
-      { new: true }
-    );
-
-    res.status(200).json(updatedPost);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
+    
+  } catch (error) {
+    
   }
 }
